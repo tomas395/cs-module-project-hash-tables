e87cb80e38cd760fe262dc0d18408c7b8833530b
Tomas Ortiz <TomasOrtiz@protonmail.com>
2020-08-05 20:40:44 -0700

    word count complete

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,41 @@
-def word_count(s):
-    # Your code here
+# x = word_count('":;,.-+=/\\|[]{}()*^&')
+# " : ; , . - + = / \ | [ ] { } ( ) * ^ &
+ignore = [":", ";", ",", ".", "-", "+", "=", "/", "\\", "|", "[", "]", "{", "}", "(", ")", "*", "^", "&", "\""]
 
+def word_count(s):
+    cache = {}
+    new_string = s.lower()
+    for c in ignore:
+        new_string = new_string.replace(c, "")
+    new_string = new_string.split()
+    for c in new_string:
+        if c in cache:
+            cache[c] += 1
+        else:
+            cache[c] = 1
+    return cache
+
+# Output
+
+# It returns a dictionary of words and their counts:
+
+# ```
+# {'hello': 2, 'my': 2, 'cat': 2, 'and': 1, "doesn't": 1, 'say': 1, 'back': 1}
+# ```
+
+# Case should be ignored. Output keys must be lowercase.
+
+# Key order in the dictionary doesn't matter.
+
+# Split the strings into words on any whitespace.
+
+# Ignore each of the following characters:
+
+# ```
+# " : ; , . - + = / \ | [ ] { } ( ) * ^ &
+# ```
+
+# If the input contains no ignored characters, return an empty dictionary.
 
 
 if __name__ == "__main__":
